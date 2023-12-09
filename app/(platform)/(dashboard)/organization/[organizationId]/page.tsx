bb900ee5ff0a63f5413dd2d0b219c8c0c1d33b92
Venci <vencislavpeev18.8@gmail.com>
2023-12-09 18:32:52 +0200

    Create and Delete Server Actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,31 +1,21 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const OrganizationIdPage=()=>{
-   async function create(formData: FormData){
-      "use server";
 
-      const title = formData.get("title") as string
-
-      await db.board.create({
-         data:{
-            title,
-         }
-      })
-   }
-// 3:29:19
+const OrganizationIdPage=async()=>{
+   const boards = await db.board.findMany();
 
    return(
-      <div>
-         <form action={create}>
-            <input 
-            id="title"
-            name="title"
-            required
-            placeholder="Enter a board title"
-            className="border-black border p-1"
-            />
-         </form>
-         
+      <div className="flex flex-col space-y-4">
+         <Form />
+      <div className="space-y-2">
+         {boards.map((board)=>(
+            <Board key={board.id} title={board.title} id={board.id}/>
+         ))}
+      </div>
       </div>
    )
 }
